e0ea7b500eb7b4acc07161454c9386176dbeaf08
波波 <apple@192.168.101.104>
2020-04-21 17:10:34 +0800

    [ dev ] - 引入 axios 和 vConsole，配置环境变量文件

--- src/main.js
+++ src/main.js
@@ -2,11 +2,32 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'vant/lib/icon/local.css' // 引入 vant 本地 icon 样式文件
+import Vconsole from 'vconsole' // 移动端调试工具
+import { Icon, Dialog, Toast } from 'vant' // 需要全局引入的 vant 组件
 
+/**
+ * vw、vh 单位的兼容性处理
+ */
 const hacks = require('viewport-units-buggyfill/viewport-units-buggyfill.hacks')
 require('viewport-units-buggyfill').init({
   hacks: hacks
 })
+
+/**
+ * 在非生产环境下，打开 vConsole 调试工具
+ */
+if (process.env.NODE_ENV !== 'production') {
+  const vConsole = new Vconsole()
+  Vue.use(vConsole)
+}
+
+/**
+ * 全局挂载 vant 组件
+ */
+Vue.use(Icon)
+Vue.use(Dialog)
+Vue.use(Toast)
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,28 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <van-button @click="onClick">按钮</van-button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import { Button } from 'vant'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    'van-button': Button
+  },
+  methods: {
+    onClick () {
+      this.$dialog({
+        title: '提示弹窗',
+        message: '提示的内容'
+      })
+    }
   }
 }
 </script>
